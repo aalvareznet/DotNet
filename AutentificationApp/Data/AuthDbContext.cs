992e4b36c4fc70ea2bce0be4d232f02856cf20ac
aalvareznet <velurn123@gmail.com>
2024-03-08 17:39:53 -0600

    Adding more

---
+++ AutentificationApp/Data/AuthDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AutentificationApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutentificationApp.Data
+{
+    public class AuthDbContext : IdentityDbContext<AppUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options):
+            base(options)
+        { 
+        
+        }
+    }
+}
---
+++ AutentificationApp/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AutentificationApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int ID { get; set; }
+        public string? Nombre { get; set; }
+        public string? Email { get; set; }
+        public string? Contraseña { get; set; }
+    }
+}
