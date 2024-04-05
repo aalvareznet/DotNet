62b49245e64faf57865d396882448eb915ebe9be
aalvareznet <velurn123@gmail.com>
2024-04-05 15:54:50 -0600

    Adding controllers

---
+++ ProyectoFinal_UI/Connected Services/srvClientes/ClientesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal_UI.srvClientes
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
