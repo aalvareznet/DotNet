9993e9f8332cee6c1d0c53d4416484709ad7f835
Anthony Alvarez Delgado <107961797+TonyAlvaDB@users.noreply.github.com>
2024-04-14 19:03:54 -0600

    Deleting not completed proyects

--- EscuelaAPI/DTOs/Materias/InsertarMateriaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EscuelaAPI.DTOs.Materias
-{
-    public class InsertarMateriaDto
-    {
-        public string? Nombre { get; set; }
-        public string? Especialidad { get; set; }
-
-    }
-}
